494f20f15442591f9bb8e6385648866329cc3440
Jan Rabe <jan.rabe@exozet.com>
2018-01-25 23:31:33 +0100

    updated demo app

--- app/src/main/java/net/kibotu/pgp/app/MainActivity.kt
+++ app/src/main/java/net/kibotu/pgp/app/MainActivity.kt
@@ -3,15 +3,14 @@ package net.kibotu.pgp.app
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils.isEmpty
+import kotlinx.android.synthetic.main.activity_main.*
 import net.kibotu.pgp.Pgp
 
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = javaClass.simpleName
-
-    var encrypted: String? = null
-    var decrypted: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +20,41 @@ class MainActivity : AppCompatActivity() {
         Pgp.setPublicKey(Pgp.genPGPPublicKey(krgen))
         Pgp.setPrivateKey(Pgp.genPGPPrivKey(krgen))
 
+        decrypted.setText("decrypted.json".openFromAssets())
+        encrypted.setText("encrypted.json".openFromAssets())
+
+        encrypt.setOnClickListener {
+            val decryptedText = decrypted.text.toString().trim()
+            if (!isEmpty(decryptedText))
+                encrypted.setText(Pgp.encrypt(decryptedText))
+        }
+
+        decrypt.setOnClickListener {
+            val encryptedText = encrypted.text.toString().trim()
+            if (!isEmpty(encryptedText))
+                decrypted.setText(Pgp.decrypt(encryptedText, "password"))
+        }
+
+        share.setOnClickListener { share() }
+        delete.setOnClickListener {
+            encrypted.setText("")
+            decrypted.setText("")
+        }
+    }
+
+    private fun encryptDecryptAssets() {
+
         Pgp.setPublicKey("rsa.pub".openFromAssets())
         Pgp.setPrivateKey("rsa".openFromAssets())
 
-        encrypted = Pgp.encrypt("decrypted.json".openFromAssets())
-        decrypted = Pgp.decrypt("encrypted.txt".openFromAssets(), "password")
+        val encrypted = Pgp.encrypt("decrypted.json".openFromAssets())
+        val decrypted = Pgp.decrypt("encrypted.txt".openFromAssets(), "password")
     }
 
-    private fun share(encrypted: String?) {
+    private fun share() {
         val intent = Intent()
         intent.action = Intent.ACTION_SEND
-        intent.putExtra(Intent.EXTRA_TEXT, encrypted)
+        intent.putExtra(Intent.EXTRA_TEXT, decrypted.text.toString().trim() + "\n\n" + encrypted.text.toString().trim())
         intent.type = "text/plain"
         startActivity(Intent.createChooser(intent, "Save Data with:"))
     }
